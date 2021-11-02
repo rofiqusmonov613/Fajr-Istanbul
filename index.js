5c40f4803decfeace3c8c588fcce8c37a98b7aa4
rofiqusmonov613 <rofiqusmonov613@gmail.com>
2021-11-03 01:11:56 +0500

    Fajr Istanbul

---
+++ index.js
@@ -0,0 +1,7 @@
+let hamburger = document.querySelector('.header__icon')
+let modal = document.querySelector('.modal')
+
+
+hamburger.addEventListener('click', function() {
+    modal.classList.toggle('ochilish')
+})
